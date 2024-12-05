5be51f79638b1f78df0dd9f39a5f60e5a5594e5b
Patrique Ouimet <patrique.ouimet@gmail.com>
2024-12-05 08:18:20 -0500

    chore: completed day 3 part 1

---
+++ day_3/part_1_test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "jsr:@std/assert";
+import { main } from "./part_1.ts";
+
+Deno.test("part 1 example test", async () => {
+  const result = await main("example.txt")
+  assertEquals(result, 161)
+})
