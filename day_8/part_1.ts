eb67485054c4956b6cdc3b42b2e67bde564fd4a1
Patrique Ouimet <patrique.ouimet@gmail.com>
2024-12-08 10:59:12 -0500

    chore: setup day 8

---
+++ day_8/part_1.ts
@@ -0,0 +1,14 @@
+export async function main(filePath?: string): Promise<number> {
+  const filePathArg = Deno.args?.[0]?.replace('--filePath=', '');
+  const filename = import.meta.dirname + "/" +
+    (filePath ?? filePathArg ?? "input.txt");
+  const text = await Deno.readTextFile(filename);
+
+  let total = 0;
+
+  return total;
+}
+
+if (import.meta.main) {
+  console.log("RESULT: " + await main());
+}
